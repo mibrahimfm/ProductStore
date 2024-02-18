3ba65a3eba5ce3d21af77199afaaee3026008625
Matheus Magalhaes <matheusibrahimfm@gmail.com>
2024-02-18 13:16:10 -0300

    Add project files.

---
+++ ProductStore/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductStore
+{
+    public class ProductRepository
+    {
+        private static ProductRepository? _instance;
+        private List<string> _products;
+
+        private ProductRepository()
+        {
+            _products = new List<string>();
+        }
+
+        public static ProductRepository Instance 
+        { 
+            get 
+            {
+                if (_instance is null)
+                    _instance = new ProductRepository();
+                return _instance;
+            } 
+        }
+
+        public void AddProduct(string product)
+        {
+            _products.Add(product);
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine("Products available: ");
+            foreach (string product in _products)
+                Console.WriteLine(product);
+        }
+
+    }
+}
---
+++ ProductStore/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ProductStore;
+
+var p1 = ProductRepository.Instance;
+var p2 = ProductRepository.Instance;
+
+p1.AddProduct("Microwave");
+p2.AddProduct("Fridge");
+p2.AddProduct("Stove");
+
+Console.WriteLine($"P1 and P2 are the same: {p1 == p2}");
+p1.DisplayProducts();
